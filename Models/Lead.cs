56b0473a3a086eedd999849f7a9307efa369edc6
[remieldy].github.io <46905440+remieldy@users.noreply.github.com>
2019-04-25 19:43:28 -0400

    lead controller1

--- Models/Lead.cs
+++ Models/Lead.cs
@@ -12,18 +12,18 @@ namespace RocketElevatorApi.Models {
     public class Lead {
         [Key]
         public long id { get; set; }
-        public long? customer_id { get; set; }
-        public string full_name { get; set; }
-        public string email { get; set; }
-        public string phone { get; set; }
-        public string business_name { get; set; }
-        public string project_name { get; set; }
-        public string department { get; set; }
-        public string project_description { get; set; }
-        public string message { get; set; }
-        //public Byte file_attachment { get; set; }
-        public DateTime created_at { get; set; }
-        public DateTime updated_at { get; set; }
-        //public ICollection<Column> Columns { get; set; }
+        // public long? customer_id { get; set; }
+        // public string full_name { get; set; }
+        // public string email { get; set; }
+        // public string phone { get; set; }
+        // public string business_name { get; set; }
+        // public string project_name { get; set; }
+        // public string department { get; set; }
+        // public string project_description { get; set; }
+        // public string message { get; set; }
+        // //public Byte file_attachment { get; set; }
+        // public DateTime created_at { get; set; }
+        // public DateTime updated_at { get; set; }
+        // //public ICollection<Column> Columns { get; set; }
     }
 }
